f3f6b78fecf7db3a70ecabee22c50455976ae3a9
sim-fpt-usmba <elaboui.hafida@gmail.com>
2020-10-13 16:26:34 +0200

    loginANDindcription

--- app/src/main/java/com/example/e_commerce_app/Login.java
+++ app/src/main/java/com/example/e_commerce_app/Login.java
@@ -1,24 +1,124 @@
 package com.example.e_commerce_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+    TextInputLayout mEmail,mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn,mCreateBtn1;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getSupportActionBar().setTitle("Login");
-    }
-    public void btn_singup(View view){
+        mEmail=findViewById(R.id.email);
+        mPassword=findViewById(R.id.password);
+        mLoginBtn=findViewById(R.id.create);
+        mCreateBtn=findViewById(R.id.liaison);
+        mCreateBtn1=findViewById(R.id.oubier);
+        fAuth=FirebaseAuth.getInstance();
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email= mEmail.getEditText().getText().toString().trim();
+                String password=mPassword.getEditText().getText().toString().trim();
 
-        startActivity(
-                new Intent(getApplicationContext(),Register.class)
-        );
+                if(TextUtils.isEmpty((email))){
+                    mEmail.setError("Email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("password is required");
+                    return;
+                }
+                //authentifacation the user
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(Login.this,new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this,"Login successufly",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+
+                        }else{
+
+                            Toast.makeText(Login.this,"Error!"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            }
+        });
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+
+            }
+        });
+        mCreateBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText resemail=new EditText((v.getContext()));
+                AlertDialog.Builder passwordResetDialog=new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Resat password?");
+                passwordResetDialog.setMessage("entrer your email to recieved reset link");
+                passwordResetDialog.setView(resemail);
+                
+                passwordResetDialog.setPositiveButton("oui", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String mail= resemail.getText().toString();
+                        fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Login.this,"Reset Link Sent To Your E-mail",Toast.LENGTH_SHORT).show();
+                                
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this,"Error ! Reset Link is Not Sent"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                passwordResetDialog.setNegativeButton("Non", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        
+                        
+                    }
+                });
+                passwordResetDialog.create().show();
+            }
+        });
 
     }
+
+
+
 }
--- app/src/main/java/com/example/e_commerce_app/Register.java
+++ app/src/main/java/com/example/e_commerce_app/Register.java
@@ -1,15 +1,182 @@
 package com.example.e_commerce_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Document;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Register extends AppCompatActivity {
+    TextInputLayout nom1,prenom1,email1,password1,phone1,adress1;
+    Button ins1;
+    FirebaseAuth fAuth;
+    RadioGroup sexe;
+    FirebaseFirestore fStore;
+    String userID;
+
+    //private static final String TAG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getSupportActionBar().setTitle("Inscrire");
+        nom1=findViewById(R.id.nom);
+        prenom1=findViewById(R.id.prenom);
+        email1=findViewById(R.id.email);
+        password1=findViewById(R.id.password);
+        phone1=findViewById(R.id.tele);
+        adress1=findViewById(R.id.adresse);
+        ins1=findViewById(R.id.ins);
+        fAuth=FirebaseAuth.getInstance();
+
+        fStore= FirebaseFirestore.getInstance();
+
+        if(fAuth.getCurrentUser()!= null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+
+
+
+        ins1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String nom= nom1.getEditText().getText().toString().trim();
+                final String prenom=prenom1.getEditText().getText().toString().trim();
+                final String email= email1.getEditText().getText().toString().trim();
+                String password=password1.getEditText().getText().toString().trim();
+               final String phone=phone1.getEditText().getText().toString().trim();
+               final String adresse=adress1.getEditText().getText().toString().trim();
+                if (TextUtils.isEmpty(nom)) {
+                    nom1.setError("nom is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(prenom)) {
+                    prenom1.setError("prenom is required");
+                    return;
+                }
+                if(TextUtils.isEmpty((email))){
+                    email1.setError("Email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    password1.setError("password is required");
+                    return;
+                }
+                if(password.length()<8){
+                    password1.setError("password Must be >= 8 characters");
+                }
+                if (TextUtils.isEmpty(phone)) {
+                    phone1.setError("phone is required");
+                    return;
+                }
+                if(phone.isEmpty() || phone.length() < 10){
+                    phone1.setError("Enter a valid mobile");
+                    phone1.requestFocus();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(adresse)) {
+                    adress1.setError("adresse is required");
+                    return;
+                }
+
+                //register the user in firebase
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+
+                            //send verification link
+                            FirebaseUser fuser=fAuth.getCurrentUser();
+                            fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                   Toast.makeText(Register.this,"verification Email Has been sent.", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("TAG", "onFailure: Email not sent"+e.getMessage());
+                                }
+                            });
+                            Toast.makeText(Register.this,"User created",Toast.LENGTH_SHORT).show();
+                            userID= fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference=fStore.collection("users").document(userID);
+
+                            Map<String, Object> user= new HashMap<>();
+
+                            user.put("nom",nom);
+                            user.put("prenom",prenom);
+                            user.put("phone",phone);
+                            user.put("email",email);
+                            user.put("Adresse",adresse);
+
+
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                   Log.d("TAG","onSuccess: user profile is  created for"+userID);
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("TAG","onFailure: "+e.toString());
+
+                                }
+                            });
+
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(Register.this,"Error!"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            }
+        });
+
+
+
+
     }
+
 }
